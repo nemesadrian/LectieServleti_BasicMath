6fa96cfad77a9f5d8e9a839799fb3e4db302d6d9
FlorianUlmeanu <florian.ulmeanu@gmail.com>
2015-09-26 12:05:28 +0300

    Post

--- src/main/java/db/TestDB.java
+++ src/main/java/db/TestDB.java
@@ -7,10 +7,21 @@ import java.sql.SQLException;
  */
 public class TestDB {
     public static void main(String[] args) {
+//        DbOps user1 = new DbOps();
+//        try {
+//            user1.signUp("user1", "parola1", "email1@yahoo.com");
+//            System.out.println("am adaugat un user");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+
         DbOps user1 = new DbOps();
         try {
-            user1.signUp("user1", "parola1", "email1@yahoo.com");
-            System.out.println("am adaugat un user");
+            user1.AddPost("Flaviu", "ana are mere, noua ne iese");
+            System.out.println("am adaugat o postare");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
